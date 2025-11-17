b3dc303ec84770701dfc2922c20cd6575bb60894
Siddhant Dixit <siddhant.dec19@gmail.com>
2025-11-17 06:46:21 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,9 @@
+print("Calculations")
+a= 44
+b=12
+print("The sum of the given numbers",(a+b))
+print("The sum of the given numbers",(a-b))
+print("The sum of the given numbers",(a*b))
+print("The sum of the given numbers",(a/b))
+print("The sum of the given numbers",(a%b))
+print("The sum of the given numbers",(a//b))
